8ccf2a84e8b45ebbe648eb5e2ab60be16e958eb0
ATHOS PINTO GUEDES NETO <athos_guedes-neto@estudante.sesisenai.org.br>
2024-09-19 16:59:46 -0300

    atualizando telas de cadastro

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,15 @@
 export function App() {
   return (
-    <header className="bg-azul h-1/4 max-h-44 min-h-28 flex justify-center">
-      <h1 className="text-4xl">SENAI</h1>
-    </header>
+    <div className="bg-slate-100">
+      <header className="bg-azul h-1/4 max-h-44 min-h-28 flex justify-center">
+        <h1 className="text-4xl">SENAI</h1>
+      </header>
+      <main className="flex justify-center py-[90px]">
+        <img src="public/SNG PNG.png" alt=""/>
+      </main>
+      <footer className="bg-azul h-20 w-full absolute bottom-0">
+        <h3>SENAI</h3>
+      </footer>
+    </div>
   )
 }
