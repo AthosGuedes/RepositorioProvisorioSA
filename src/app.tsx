5a85b86d11853f2d60603b7f08f7135f3af05292
AthosGuedes <athosgneto@gmail.com>
2024-09-25 12:06:42 -0300

    background feito, font 'inter' adicionada ao projet inteiro

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,15 @@
 export function App() {
   return (
-    <div>
-      <div className="flex justify-end">
-        <div className="h-full w-[120px] bg-azul-500">
-          <h1 className="">senai</h1>
-        </div>
+    <div className="h-screen">
+      <div className=" flex justify-end h-full -z-10 pr-2">
+        <div className="top-0 h-full w-32 mr-2 bg-azul-500"></div>
+        <div className="top-0 h-full w-32 mr-2 bg-azul-500"></div>
+        <div className="top-0 h-full w-32 mr-2 bg-azul-600"></div>
+        <div className="top-0 h-full w-32 mr-2 bg-azul-600"></div>
+        <div className="top-0 h-full w-32 mr-2 bg-azul-700"></div>
+        <div className="top-0 h-full w-32 mr-2 bg-azul-700"></div>
+        <div className="top-0 h-full w-32 mr-2 bg-azul-800"></div>
+        <div className="top-0 h-full w-32 mr-2 bg-azul-800"></div>
       </div>
     </div>
   )
