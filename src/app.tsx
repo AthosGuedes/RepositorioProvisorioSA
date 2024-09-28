9778e072ff24c786edc9c6389198c666bae17915
AthosGuedes <athosgneto@gmail.com>
2024-09-27 23:42:24 -0300

    instalado biblioteca lucide-icons e tela de login quase finalizada

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
+import { User, KeyRound, Eye } from "lucide-react"
 
 export function App() {
 
-  const [isModalLoginOpen, setIsModalLoginOpen] = useState(false)
+  const [isModalLoginOpen, setIsModalLoginOpen] = useState(true)
 
   return (
     <div className="h-screen">
-      {isModalLoginOpen ? ( // 4 listras tela login
+      {isModalLoginOpen ? ( // 4 listras - tela login
         <div className="h-screen">   
           <div className=" flex justify-end h-full -z-10 pr-10">
             <div className="top-0 h-full w-32 mr-2 bg-azul-500"></div>
@@ -15,7 +16,7 @@ export function App() {
             <div className="top-0 h-full w-32 mr-2 bg-azul-800"></div>
           </div>
         </div>
-       ) : ( // 8 listras tela acessar
+       ) : ( // 8 listras - tela acessar
         <div className="h-screen">
           <div className=" flex justify-end h-full -z-10 pr-2">
             <div className="top-0 h-full w-32 mr-2 bg-azul-500"></div>
@@ -36,16 +37,28 @@ export function App() {
           <img src="/SESI-SENAI-LOGO.png" alt="Senai e Sesi - juntos pela industria" className="w-[750px] h-auto"/>
         </div>
         <div className="flex justify-end items-center fixed top-0 right-20 w-full h-full z-0">
-          <div className="flex justify-center items-center flex-col bg-white bg-opacity-95 w-[650px] h-[800px] rounded-3xl p-5 relative gap-12">
+          <div className="flex justify-center items-center flex-col bg-white bg-opacity-95 w-[650px] h-[800px] rounded-3xl p-5 relative">
             <h3 className="text-6xl text-azul-600 font-extrabold">Login</h3>
-            <form>
-              <div>
-                <label htmlFor=""></label>
-                <input type="text" />
+            <form className="mt-16 mb-10">
+              <div className="mb-6">
+                <label htmlFor="username" className="block mb-1 text-xl font-medium text-black">Usuário: </label>
+                <div className=" flex items-center w-[400px] h-20 rounded-lg bg-slate-200">
+                  <User className="fixed z-10 ml-5"></User>
+                  <input type="text" id="username" className="w-[400px] h-20 rounded-lg bg-transparent px-14" placeholder="Informe o nome de usuário"/>
+                </div>
               </div>
-
+              <div>
+                <label htmlFor="password" className="block mb-1 text-xl font-medium text-black">Senha: </label>
+                <div className=" flex items-center w-[400px] h-20 rounded-lg bg-slate-200">
+                  <KeyRound className="fixed z-10 ml-5"></KeyRound>
+                  <input type="password" id="password" className="w-[400px] h-20 rounded-lg bg-transparent px-14 " placeholder="Digite a senha"/>
+                  <button className="fixed z-10 ml-[348px]">
+                    <Eye></Eye>
+                  </button>
+                </div>
+              </div>
             </form>
-            <button className="w-[450px] h-16 bg-azul-600 hover:bg-azul-700 text-4xl text-white">Entrar</button>
+            <button className="w-[400px] h-20 rounded-lg bg-azul-600 hover:bg-azul-700 text-4xl text-white">Entrar</button>
           </div>
         </div>
       </div>
